4485f81569461284f94a6a1144197581bae35b71
Daniel Monzon <monzondaniel1@gmail.com>
2023-04-04 20:05:26 -0700

    updated load_tweets_batch.py

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -15,10 +15,10 @@ time echo "$files" | parallel ./load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-#echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
+time echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2373/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-#echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+time echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:2374/ --inputs
 
