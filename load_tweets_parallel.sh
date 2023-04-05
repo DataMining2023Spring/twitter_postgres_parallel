3b11b254e41c6d1d3eb1f5807e2b0fbbc9b794ab
Daniel Monzon <monzondaniel1@gmail.com>
2023-04-04 18:42:27 -0700

    parallel denormalized done

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,19 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this
+#time for file in $files; do
+    ./load_denormalized.sh "$file"
+    #time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:2372/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+#done
+time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
+#echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+#echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+
